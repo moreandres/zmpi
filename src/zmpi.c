66ee2b0efc8718bc40a4285c0be8caa28818f61e
Andres More <more.andres@gmail.com>
2013-06-08 21:16:52 -0300

    MPI_Get_version() and MPI_Wtime()

--- src/zmpi.c
+++ src/zmpi.c
@@ -4,6 +4,27 @@
 
 #include "zmpi.h"
 
+int MPI_Get_version(int *version, int *subversion)
+{
+  if (!version || !subversion)
+    return MPI_ERR_ARG;
+
+  *version = MPI_VERSION;
+  *subversion = MPI_SUBVERSION;
+
+  return MPI_SUCCESS;
+}
+
+double MPI_Wtime(void)
+{
+  double sec;
+  struct timeval tv;
+
+  gettimeofday(&tv, NULL);
+  sec = tv.tv_sec + tv.tv_usec / 1000000.0;
+  return sec;
+}
+
 int main(int argc, char *argv[])
 {
   return EXIT_SUCCESS;
